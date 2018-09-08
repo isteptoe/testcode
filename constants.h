1ac29563412ff6f34e378801a7367ff050fe1bc1
Ian Steptoe <40774387+isteptoe@users.noreply.github.com>
2018-09-08 11:31:00 -0400

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,47 @@
+#ifndef CONSTANTS
+
+#define CONSTANTS
+
+//sorter constants
+const int ORANGE = 135;
+const int PICK_UP = 98;
+const int WHITE = 65;
+const int BALL_TRIGGER = 5000;
+const int SORT_TIME = 50;
+
+//speed constants
+const int SLOW_SPEED = 50;
+const int HALF_SPEED = 100;
+const int FULL_SPEED = 150;
+
+//Joel constants
+const int TARGET_INDEX = 3;
+const int TURN_AMOUNT  = 3;
+const char LEFT = 'l';
+const char RIGHT = 'r';
+const char FORWARD = 'f';
+const char BACK = 'b';
+
+// Dasch constants
+const int  BLUETOOTH_LIMITER = 4000;
+const bool WHEEL_FORWARDS   = true;
+const bool WHEEL_BACKWARDS  = false;
+const bool FORWARDS         = true;
+const bool BACKWARDS        = false;
+
+
+const int DO_DUMP = 95;
+const int DONT_DUMP = 200;
+
+
+const int ARM_RIGHT = 150;
+const int ARM_MIDDLE = 100;
+const int ARM_LEFT = 50;
+// Paths
+
+
+
+
+
+
+#endif
---
+++ pins.h
@@ -0,0 +1,32 @@
+// LEDs
+const int LEDY = 38;
+const int LEDG = 40;
+
+// Buttons
+const int BUTTON1 = 30;
+const int BUTTON2 = 32;
+
+// Wheels
+const int WHEEL_DIR_LB = 26;
+const int WHEEL_DIR_LF = 28;
+const int WHEEL_DIR_RB = 34;
+const int WHEEL_DIR_RF = 36;
+
+const int WHEEL_SPEED_L = 7;
+const int WHEEL_SPEED_R = 8;
+
+const int WHEEL_STBY = 33;
+
+// Line sensors
+int LINE_SENSOR[] = {37, 39, 41, 43, 45, 47, 49, 51};
+int FORK_SENSOR = ##;
+
+//Analog
+const int FRONT_RIGHT_SENSOR = A0;
+const int FRONT_LEFT_SENSOR = A1;
+
+// Servos
+const int DUMPER   = 9;
+
+const int ARM   = 13;
+
